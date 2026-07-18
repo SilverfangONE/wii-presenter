a37df604223183c11ae98358e29cdd066550820f
SilverfangONE <luis.boden@gmail.com>
2026-07-18 14:37:12 +0200

    remove dead code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use wii_presenter::wiimote::Wiimote;
-
-// --- 3. DIE MAIN FUNKTION (Der Kontrollfluss) ---
 fn main() {
-    let wiimote: Box<dyn Wiimote>;
-
-    #[cfg(target_os = "windows")]
-    {
-
-    }
-
-
- 
+    println!("start wii-presenter");
 }
